bf8114c3a88adf73e6d4fc3ee956c01cd0fbdf57
Harald Reingruber <harald.reingruber@dedalus-group.com>
2021-05-21 15:45:49 +0200

    RED: First session of dna-transcription-kata, ending with failing test (2 ignored)
    Co-Authored-By: Daeun Kim <79833541+daeun-kim21@users.noreply.github.com>
    Co-Authored-By: Kristóf Juhász <76943150+kristof-juhasz@users.noreply.github.com>
    Co-Authored-By: MariettaSauerzapf <76515963+mariettasauerzapf@users.noreply.github.com>
    Co-Authored-By: Mauro Ceraso <76943029+mauroceraso@users.noreply.github.com>
    Co-Authored-By: Norbert Toth <75730727+norbertgabortoth@users.noreply.github.com>

--- js/jest-tests/src/HelloWorld.js
+++ js/jest-tests/src/HelloWorld.js
@@ -1,2 +1,22 @@
 // Enjoy arrow functions
-export default () => 'Hello World!';
+//export default () => 'Hello World!';
+
+
+
+ export default class DNAExample {
+    static switchAT(name) {
+        if (name === 'a')  
+            return 't';
+        if (name === 't') 
+            return 'a';
+        return null;
+    }
+    
+    static reverse(sequence) {
+        return sequence.split("").reverse().join("");
+    }
+
+    // static translate(sequence) {
+
+    // }
+ }
--- js/jest-tests/test/HelloWorld.test.js
+++ js/jest-tests/test/HelloWorld.test.js
@@ -1,5 +1,34 @@
-import HelloWorld from '../src/HelloWorld';
+import DNAExample from "../src/HelloWorld";
 
-test('Hello World should return "Hello World!"', () => {
-  expect(HelloWorld()).toBe('Oh oh ...');
+test("a should return t", () => {
+  var a = "a";
+  expect(DNAExample.switchAT(a)).toBe("t");
 });
+
+test("t should return a", () => {
+  var t = "t";
+  expect(DNAExample.switchAT(t)).toBe("a");
+});
+
+test("c should return g", () => {
+  var str = "c";
+  expect(DNAExample.switchCG(str)).toBe("g");
+});
+
+test('a DNA sequence should be reversed', () => {
+  let testString = 'TTAGGGCATG';
+  let expectedString = testString.split("").reverse().join("");
+  expect(DNAExample.reverse(testString)).toBe(expectedString);
+})
+
+test("big example string should be correctly translated", () => {
+  var bigString = "AGGACGGGCTAACTCCGCTCGTCACAAAGCGCAATGCAGCTATGGCAGATGTTCATGCCG";
+  // expect(DNAExample.reverse(bigString)).toBe("UCC UGC CCG AUU GAG GCG AGC AGU GUU UCG CGU UAC GUC GAU ACC GUC UAC AAG UAC GGC");
+});
+
+test("small example string should be correctly translated", () => {
+  let testString = 'TTAGGGCATG';
+  let expectedString = 'CATGCCCTAA';
+  //expect(DNAExample.translate(testString)).toBe(expectedString);
+});
+
